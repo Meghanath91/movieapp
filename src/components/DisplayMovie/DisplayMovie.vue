2a4eeee3f9223708185d941fc3639d3c25bf5b0e
Meghanath91 <meghanath.balaji@gmail.com>
2020-09-16 00:45:50 -0400

    display movie component changed

--- src/components/DisplayMovie/DisplayMovie.vue
+++ src/components/DisplayMovie/DisplayMovie.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="result-item-container">
     <div>
-      <img v-bind:src="'http://image.tmdb.org/t/p/w500/'+movie.poster_path" width="130px" />
+      <img
+        v-if="movie.poster_path"
+        v-bind:src="'http://image.tmdb.org/t/p/w500/'+movie.poster_path"
+        width="130px"
+      />
+      <img v-else src="/assets/no_image.jpg" width="130px" />
     </div>
     <div class="details-container">
       <h3>{{movie.title}}</h3>
@@ -14,8 +19,57 @@
       <p>{{movie.overview}}</p>
       <div class="btn-container">
         <p>View Details</p>
-        <input type="button" value="Add movie to favorites" />
+        <input @click="addMovie" type="button" value="Add movie to favorites" />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  name: "DisplayMovie",
+  props: ["movieData", "movie", "movies"],
+
+  methods: {
+    addMovie(e) {
+      e.preventDefault();
+
+      if (this.movies.length < 15) {
+        const newMovie = {
+          id: uuidv4(),
+          data: this.movie,
+        };
+        this.movies.push(newMovie);
+        this.$emit("add-movie", newMovie);
+      } else {
+        alert("Maximum 15 movies allowed");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.result-item-container {
+  display: flex;
+  border-radius: 25px;
+}
+.result-item-container img {
+  border-radius: 25px;
+}
+.details-container {
+  margin-left: 3%;
+}
+.btn-container {
+  margin-top: 4%;
+  display: flex;
+  justify-content: space-around;
+}
+.green {
+  color: greenyellow;
+}
+.red {
+  color: red;
+}
+</style>
