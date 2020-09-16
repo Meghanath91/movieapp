ce241b966c7de1547cca85e190c6c6be6076c95c
Meghanath91 <meghanath.balaji@gmail.com>
2020-09-15 20:44:44 -0400

    add input fields for profiles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header />
+    <ProfileForm />
   </div>
 </template>
 
 <script>
 import Header from "./components/Layout/Header";
+import ProfileForm from "./components/Form/ProfileForm";
 export default {
   name: "App",
   components: {
     Header,
+    ProfileForm,
   },
 };
 </script>
---
+++ src/components/Form/ProfileForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <input type="text" v-model="firstName" name="firstName" placeholder="First name" />
+    <input type="text" v-model="lastName" name="lastName" placeholder="Last name" />
+    <textarea placeholder="Short Bio" v-model="shortBio" />
+    <p>characters left</p>
+  </form>
+</template>
+<script>
+export default {
+  name: "ProfileForm",
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      shortBio: "",
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
